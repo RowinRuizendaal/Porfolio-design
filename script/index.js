5c2a35bb82d9f9a3e6e9fa77558c9fa2391ae0ea
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2021-01-03 16:39:53 +0100

    UPDATE BUG

--- script/index.js
+++ script/index.js
@@ -51,3 +51,14 @@ const logo = document.querySelectorAll('#logo path')
 for (i = 0; i < logo.length; i++) {
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`)
 }
+
+
+const toggle = () => {
+    const menu = document.querySelector('.toggle')
+    const navigation = document.querySelector('.navigation')
+
+    menu.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+
+document.querySelector('.toggle').addEventListener('click', toggle)
